a519470cdc79e8ee9c7e3621abbd2b53a18561d2
shivammaurya22 <shivmmaurya2292005@gmail.com>
2025-03-12 08:03:19 +0530

    Done New files are added.

---
+++ PrintsF.java
@@ -0,0 +1,16 @@
+public class PrintsF{
+    public static void main(String[] args){
+        boolean mybool = true;  // %b for boolean
+        char mychar = '@';      // %c for char
+        String mystring = "Shivam";  //%s for string
+        int myint = 24;              // %d means decimal for integers
+        double mydouble = 1000;   //%f means float for double values
+        // main method 
+       // System.out.printf("%b", mybool);
+       // System.out.printf("%c", mychar);
+      //  System.out.printf("%s", mystring);
+      //  System.out.printf("%d", myint);
+        System.out.printf("%f", mydouble);
+       // System.out.printf("%d This is the formet string ", 123);
+    }
+}
