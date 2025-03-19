d5e1c4abb716e677ee5c98c0809652fcf202c171
shivammaurya22 <shivmmaurya2292005@gmail.com>
2025-03-19 22:33:41 +0530

    Done

---
+++ Methods02.java
@@ -0,0 +1,12 @@
+class Person{
+    public void canRun(){
+        System.out.println("Running...");
+    }
+}
+public class Methods02 {
+    public static void main(String[] args){
+        // Main Method
+        Person per = new Person();
+        per.canRun();
+    }
+}
